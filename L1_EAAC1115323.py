49322f42e8006b466f6ca2d8fb38f738e78c6bc6
Steph-arana22 <143531094+Steph-arana22@users.noreply.github.com>
2023-09-05 10:01:57 -0600

    Phyton
    Laboratorio No.1 
    Diferencia de print y print end=""

---
+++ L1_EAAC1115323.py
@@ -0,0 +1,22 @@
+'''print ("Hola Mundo soy Nombre")
+print ("Hola Mundo soy Estephanie")
+#print ("Hola Mundo soy Estephanie")
+
+print("Hola Mundo")
+print("soy Estephanie")
+
+print("Hola Mundo", end="")
+print("soy Estephanie", end="")'''
+#print muestra el mensaje tal y como se escribe con salto de línea, mientras que print con ,end="" al final del mensaje, muestra el mensaje junto con el otro que se escribió de bajo de él.
+
+'''texto= input()
+print(texto + "Hola")'''
+
+print("Ingrese su nombre:")
+Nombre=input()
+
+print("Hola mundo")
+print("soy "+ Nombre)
+
+print("Hola mundo ", end="")
+print("soy "+ Nombre, end="")
